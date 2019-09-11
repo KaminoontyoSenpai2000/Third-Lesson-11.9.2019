1a7a4e2ea530e55dbb227b0442885e60f4962ef5
Ivan <49728539+KaminoontyoSenpai2000@users.noreply.github.com>
2019-09-11 21:14:38 +0300

    Add files via upload

---
+++ 3. Third Lesson class work 11 09 2019/main.js
@@ -0,0 +1,86 @@
+// ES5
+
+/*var a = 13;
+var b;
+var a1, a2, a3;
+
+console.log(a1);
+
+a1 = 10;
+
+console.log(a1);*/
+
+
+
+/*var h= 10; 
+var w = 12;
+
+var perimeter = 2 * (h + w);
+
+console.log('Perimeter = ' + perimeter);*/
+
+
+
+/*var i = 234;  // integer 
+var f = 23.64; // float double
+var b = true; // boolean
+var s = 'test'; //string
+
+var Is = `string
+
+with
+		new
+lines`
+
+console.log(Is);
+
+console.log(b);
+console.log(s);*/
+
+//***"var" - это объявление переменной!!!
+
+
+// ES6 ... ES2017 ES2018
+
+/*let x = 12;
+x = '13';*/
+
+//const y = 1; // только один раз можно присвоить к константе!
+
+/*let a = 10;
+let b = 20;
+let s2 = `Values: a = ${a}; b = ${b};`;
+console.log(s2);*/
+
+
+
+/*let a = 7; 
+let b = 8;
+
+let r = a < b;
+
+console.log(r);*/
+
+
+/*let a = 1; 
+let b = 4;
+
+if (a < b) {
+	console.log("a < b");
+} else {
+	console.log("a >= b");
+}*/
+
+
+let a = 7;
+let b = 4;
+
+let max = 0;
+
+if (a < b) {
+	max = b;
+} else {
+	max = a;
+}
+
+console.log(max);
